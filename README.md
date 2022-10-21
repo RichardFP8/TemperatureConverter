48c007c7e58076544f5ff8664984fa23cfabd223
Richard Perez <richard.floresperez0@gmail.com>
2022-10-20 21:40:11 -0400

    I rushed and missed some requirements, now I'm closer to finish

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TemperatureConverter
 Calculating conversion of units : Celsius &amp; Fahrenheit
+# 3 pages: Home, Celsius to Fahrenheit, Fahrenheit to Celsius 
---
+++ scripts/celsius.js
@@ -0,0 +1,17 @@
+//converting celsius to fahrenheit
+"use strict";
+//interactions occur after window finishes loading
+window.onload = init;
+
+//init triggers the function that converts input 
+function init() {
+    const convert = document.getElementById("convertC");
+    convert.onclick = conversionC_toF;
+}
+//access, extract, change input; compute and return number 
+function conversionC_toF(){
+    const getCelsius = Number(document.getElementById("celsius").value);
+    const answer = document.getElementById("toFahrenheit");
+    let toFahrenheit = ( (9 / 5) * getCelsius ) + 32 ;
+    answer.value = toFahrenheit.toFixed(2);
+}
